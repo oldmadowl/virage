7e5d39c2f37401f0eca304d1025b4883b53321cf
oldmadowl <oldmadowl@gmail.com>
2022-06-14 00:23:58 +0300

    added header

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,15 @@
-$(function(){
+$(function () {
 
-  
+
+  // animation for drop menu
+  $('.drop-menu-link').hover(function () {
+    $(this).toggleClass('drop-menu-link--active');
+    $('.drop-menu').toggleClass('drop-menu--active');
+  });
+
+  $('.drop-menu').hover(function () {
+    $(this).toggleClass('drop-menu--active');
+    $('.drop-menu-link').toggleClass('drop-menu-link--active');
+  });
 
 });
